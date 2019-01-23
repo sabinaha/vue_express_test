97737cdbad0a7291b691344f08c49402ac332146
Sabina <sabinaha@kth.se>
2019-01-23 15:17:12 +0100

    Fixed typo

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const posts = require('./routes/api/posts');
 app.use('/api/posts', posts);
 
 // Handle production
-if (process.env.NODE_ENV == 'production') {
+if (process.env.NODE_ENV === 'production') {
     // static folder
     app.use(express.static(__dirname + '/public/'));
 
